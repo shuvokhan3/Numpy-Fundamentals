f20016fcf77305a4685b4d19415f5b59e930bb90
shuvokhan3 <shuvoj360@gmail.com>
2026-04-09 13:12:15 +0600

    check element exits zero

--- ProblemSolve/checkElementExitszero.py
+++ ProblemSolve/checkElementExitszero.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-array = np.array([[[1, 0], [3, 4]], [[5, 6], [7, 8]]])
+array = np.array([[[1, 0], [3, 4]], [[np.nan, 6], [7, 8]]])
 print(array.all())
 
+print(np.isfinite(array))
+
+new_array = np.identity(4)
+print(new_array)
